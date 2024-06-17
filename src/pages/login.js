a2a1576cd543ecfdbefdfda7a97df3ff22409c16
Gautam <gautamnjaisur@gmail.com>
2024-06-18 02:31:40 +0530

    commited

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,35 +1,42 @@
-import {Form,Input,Button, Anchor} from 'antd'
+import {Form,Input,Button} from 'antd'
 import {React} from 'react'
+import {Link} from 'react-router-dom';
 
-const Login = () => {
-  return (
-    <>
-     <h2 className='card-title ' >Login</h2>
+
+
+function Login ()  {
+    const onFinish= (values)=>{
+      console.log("received data",values);
+    }
+
+  return (<>
+     <h2 className='card-title '>Login Page</h2>
     <div className="authentication p-5">
     <div className="authentication-form card p-3">
 
-      <Form layout='vertical'>
-      
+      <Form layout='vertical' onFinish={onFinish}>
+     
         <Form.Item label="Email" name='email'>
-          <Input placeholder="Email"/>
+          <Input placeholder="Email" type='email'/>
         </Form.Item>
-        <Form.Item label="Password" name='password'>
-          <Input placeholder="Password"/>
+        <Form.Item label="Password" name='password' >
+          <Input placeholder="Password" type='password'/>
         </Form.Item>
 
-        <Button className="primary-button mt-3">
-          Register
-        </Button>
-      
-        <Button className="primary-button mt-3" target='/login'>
+        <Button className="primary-button my-3" htmlType='submit'>
           Login
         </Button>
+    
 
+        <Link to='/register' className='redirect mt-3'>click here to Register</Link>
+
+      
       </Form>
+
     </div>
       
     </div>
-</>
+    </>
   )
 }
 
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,30 +1,37 @@
 import {Form,Input,Button} from 'antd'
 import {React} from 'react'
+import {Link} from 'react-router-dom';
 
-const Register = () => {
+
+
+function Register ()  {
+    const onFinish= (values)=>{
+      console.log("received data",values);
+    }
+
   return (<>
      <h2 className='card-title '>Register Page</h2>
     <div className="authentication p-5">
     <div className="authentication-form card p-3">
 
-      <Form layout='vertical'>
+      <Form layout='vertical' onFinish={onFinish}>
         <Form.Item label="Name" name='name'>
           <Input placeholder="Name"/>
         </Form.Item>
         <Form.Item label="Email" name='email'>
-          <Input placeholder="Email"/>
+          <Input placeholder="Email" type='email'/>
         </Form.Item>
-        <Form.Item label="Password" name='password'>
-          <Input placeholder="Password"/>
+        <Form.Item label="Password" name='password' >
+          <Input placeholder="Password" type='password'/>
         </Form.Item>
 
-        <Button className="primary-button mt-3">
+        <Button className="primary-button my-3" htmlType='submit'>
           Register
         </Button>
-      
-        <Button className="primary-button mt-3" target='/login'>
-          Login
-        </Button>
+    
+
+        <Link to='/login' className='redirect mt-3'>click here to login</Link>
+
       
       </Form>
 
