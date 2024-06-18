167fb8f3169f65bbf336d428d9b60ca249da82a8
Gautam <gautamnjaisur@gmail.com>
2024-06-18 17:31:48 +0530

    commited

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,30 @@
-import React from 'react' 
-import axios from 'axios' ;
-const Home = () => {
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+function Home() {
+  const getData = async () => {
+    try {
+      const response = await axios.get('/api/user/get-user-info-by-id', {
+        headers: {
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        },
+      });
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
-      <h2>this is home page</h2>
+      This is Home Page
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
+
